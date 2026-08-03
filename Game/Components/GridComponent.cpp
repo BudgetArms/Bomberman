873345ba4391c07b3ce08b64831f326ac8ff4bea
BudgetArms <67414205+BudgetArms@users.noreply.github.com>
2026-08-03 19:01:16 +0200

    Added GridComponent

---
+++ Game/Components/GridComponent.cpp
@@ -0,0 +1,306 @@
+#include "GridComponent.hpp"
+
+#include "Base/LevelGridGraph.hpp"
+#include "Core/HelperFunctions.hpp"
+
+
+using namespace Game;
+
+
+GridComponent::GridComponent(bae::GameObject& owner, int columns, int rows, const glm::ivec2& cellSize) :
+    Component(owner),
+    m_LevelGridGraph{ std::make_unique<LevelGridGraph>(owner.GetWorldLocation(), columns, rows, cellSize) },
+    m_AStar{ m_LevelGridGraph.get(), bae::Graphs::HeuristicFunctions::Chebyshev }
+{
+}
+
+GridComponent::GridComponent(bae::GameObject& owner, const glm::vec2& gridSize, const int columns,
+                             const int rows) :
+    GridComponent(owner, columns, rows,
+                  glm::ivec2(static_cast<float>(gridSize.x) / static_cast<float>(columns),
+                             static_cast<float>(gridSize.y) / static_cast<float>(rows)))
+{
+}
+
+GridComponent::~GridComponent() = default;
+
+
+void GridComponent::Render() const
+{
+    m_LevelGridGraph->Render();
+}
+
+
+void GridComponent::AddNode(const int nodeId) const
+{
+    // Gets existing (grid locked) node position
+    const glm::vec2 positionNode                 = m_LevelGridGraph->GetNodePos(nodeId);
+    const bae::Graphs::GridPosition gridPosition = m_LevelGridGraph->GetGridPosition(nodeId);
+
+    if(m_LevelGridGraph->IsWithinBounds(gridPosition))
+    {
+        m_LevelGridGraph->AddNode(std::make_unique<bae::Graphs::Node>(positionNode));
+    }
+}
+
+void GridComponent::AddNode(const bae::Graphs::GridPosition& position) const
+{
+    if(!m_LevelGridGraph->IsWithinBounds(position))
+    {
+        std::cout << FUNCTION_NAME << " Failed! GridPosition not in Grid!" << '\n';
+        return;
+    }
+
+    const int nodeId = m_LevelGridGraph->GetNodeId(position);
+    AddNode(nodeId);
+}
+
+void GridComponent::AddNodeAtPosition(const glm::vec2& positionNode) const
+{
+    const int nodeId = m_LevelGridGraph->GetNodeIdAtPosition(positionNode);
+    AddNode(nodeId);
+}
+
+void GridComponent::RemoveNode(const int nodeId) const
+{
+    m_LevelGridGraph->RemoveNode(nodeId);
+}
+
+void GridComponent::RemoveNode(const bae::Graphs::GridPosition& position) const
+{
+    if(!m_LevelGridGraph->IsWithinBounds(position))
+    {
+        std::cout << FUNCTION_NAME << " Failed! GridPosition not in Grid!" << '\n';
+        return;
+    }
+
+    const int nodeId = m_LevelGridGraph->GetNodeId(position);
+    RemoveNode(nodeId);
+}
+
+void GridComponent::RemoveNodeAtPosition(const glm::vec2& positionNode) const
+{
+    const int nodeId = m_LevelGridGraph->GetNodeIdAtPosition(positionNode);
+    RemoveNode(nodeId);
+}
+
+
+void GridComponent::AddConnection(const int nodeId, const Direction direction) const
+{
+    const bae::Graphs::GridPosition gridPosition = m_LevelGridGraph->GetGridPosition(nodeId);
+
+    std::vector<glm::vec2> directions{};
+    switch(direction)
+    {
+        case Direction::Left:
+            directions.emplace_back(-1, 0);
+            break;
+        case Direction::Right:
+            directions.emplace_back(1, 0);
+            break;
+        case Direction::Up:
+            directions.emplace_back(0, -1);
+            break;
+        case Direction::Down:
+            directions.emplace_back(0, 1);
+            break;
+    }
+
+    m_LevelGridGraph->AddConnectionsInDirections(nodeId, gridPosition, directions);
+}
+
+void GridComponent::AddConnection(const glm::vec2& positionNode, const Direction direction) const
+{
+    const int nodeId = m_LevelGridGraph->GetNodeIdAtPosition(positionNode);
+    AddConnection(nodeId, direction);
+}
+
+void GridComponent::RemoveConnection(const int nodeId, const Direction direction) const
+{
+    const bae::Graphs::GridPosition gridPosition = m_LevelGridGraph->GetGridPosition(nodeId);
+
+    std::vector<glm::vec2> directions{};
+    switch(direction)
+    {
+        case Direction::Left:
+            directions.emplace_back(-1, 0);
+            break;
+        case Direction::Right:
+            directions.emplace_back(1, 0);
+            break;
+        case Direction::Up:
+            directions.emplace_back(0, -1);
+            break;
+        case Direction::Down:
+            directions.emplace_back(0, 1);
+            break;
+    }
+
+    m_LevelGridGraph->RemoveConnectionsInDirections(nodeId, gridPosition, directions);
+}
+
+void GridComponent::RemoveConnection(const glm::vec2& positionNode, const Direction direction) const
+{
+    const int nodeId = m_LevelGridGraph->GetNodeIdAtPosition(positionNode);
+    RemoveConnection(nodeId, direction);
+}
+
+void GridComponent::AddConnectionsToNeighbors(const bae::Graphs::GridPosition position) const
+{
+    m_LevelGridGraph->AddConnectionsToAdjacentCells(position);
+}
+
+bool GridComponent::GetRenderCells() const
+{
+    return m_LevelGridGraph->m_bRenderCells;
+}
+
+void GridComponent::SetRenderCells(const bool bRenderCells) const
+{
+    m_LevelGridGraph->m_bRenderCells = bRenderCells;
+}
+
+bool GridComponent::GetRenderNodes() const
+{
+    return m_LevelGridGraph->m_bRenderNodes;
+}
+
+void GridComponent::SetRenderNodes(const bool bRenderNodes) const
+{
+    m_LevelGridGraph->m_bRenderNodes = bRenderNodes;
+}
+
+bool GridComponent::GetRenderConnections() const
+{
+    return m_LevelGridGraph->m_bRenderConnections;
+}
+
+void GridComponent::SetRenderConnections(const bool bRenderConnections) const
+{
+    m_LevelGridGraph->m_bRenderConnections = bRenderConnections;
+}
+
+bae::Graphs::GridPosition GridComponent::GetGridPosition(const glm::vec2& position) const
+{
+    return m_LevelGridGraph->GetGridPosition(position);
+}
+
+glm::vec2 GridComponent::GetPosition(const bae::Graphs::GridPosition gridPosition) const
+{
+    return m_LevelGridGraph->GetPosition(gridPosition);
+}
+
+bool GridComponent::IsValidGridPosition(const bae::Graphs::GridPosition gridPosition) const
+{
+    if(!m_LevelGridGraph->IsWithinBounds(gridPosition))
+    {
+        return false;
+    }
+    const bae::Graphs::Node* node = m_LevelGridGraph->GetNode(gridPosition);
+    if(!node || !node->IsValid())
+    {
+        return false;
+    }
+
+    return m_LevelGridGraph->GetNodeId(gridPosition) != bae::Graphs::InvalidNodeID;
+}
+
+bae::Graphs::GridPosition GridComponent::GetClosestValidNodePosition(const glm::vec2& position) const
+{
+    return m_LevelGridGraph->GetClosestValidNodePositionAtPosition(position);
+}
+
+int GridComponent::GetColumns() const
+{
+    return m_LevelGridGraph->GetColumns();
+}
+
+int GridComponent::GetRows() const
+{
+    return m_LevelGridGraph->GetRows();
+}
+
+
+bool GridComponent::IsInGrid(const glm::vec2& position) const
+{
+    const bae::Graphs::GridPosition gridPosition = m_LevelGridGraph->GetGridPosition(position);
+    return IsInGrid(gridPosition);
+}
+
+bool GridComponent::IsInGrid(const bae::Graphs::GridPosition& gridPosition) const
+{
+    return m_LevelGridGraph->IsWithinBounds(gridPosition);
+}
+
+bool GridComponent::DoesConnectionExistInDirection(const bae::Graphs::GridPosition gridPosition,
+                                                   const Direction direction) const
+{
+    if(!IsInGrid(gridPosition))
+    {
+        return false;
+    }
+
+    bae::Graphs::GridPosition newGridPosition = gridPosition;
+    switch(direction)
+    {
+        case Direction::Left:
+            --newGridPosition.Column;
+            break;
+        case Direction::Right:
+            ++newGridPosition.Column;
+            break;
+        case Direction::Up:
+            --newGridPosition.Row;
+            break;
+        case Direction::Down:
+            ++newGridPosition.Row;
+            break;
+    }
+
+    if(!IsInGrid(newGridPosition))
+    {
+        return false;
+    }
+
+    const int nodeId    = m_LevelGridGraph->GetNodeId(gridPosition);
+    const int newNodeId = m_LevelGridGraph->GetNodeId(newGridPosition);
+
+    const bae::Graphs::Connection* const connection = m_LevelGridGraph->FindConnection(nodeId, newNodeId);
+    if(!connection || !connection->IsValid())
+    {
+        return false;
+    }
+
+    return true;
+}
+
+std::vector<glm::vec2> GridComponent::GetALlConnections() const
+{
+    auto nodes = m_LevelGridGraph->GetActiveNodes();
+
+    return bae::Graphs::ConvertNodesToPositions(nodes);
+}
+
+
+std::vector<glm::vec2> GridComponent::GetShortestPath(const int startNodeId, const int endNodeId) const
+{
+    const bae::Graphs::GridPosition startPos =
+            m_LevelGridGraph->GetClosestValidNodePosition(m_LevelGridGraph->GetGridPosition(startNodeId));
+    const bae::Graphs::GridPosition endPos =
+            m_LevelGridGraph->GetClosestValidNodePosition(m_LevelGridGraph->GetGridPosition(endNodeId));
+
+    const std::vector<bae::Graphs::Node*> pathNodes = m_AStar.FindPath(m_LevelGridGraph->GetNode(startPos),
+                                                                       m_LevelGridGraph->GetNode(endPos));
+
+    return bae::Graphs::ConvertNodesToPositions(pathNodes);
+}
+
+
+std::vector<glm::vec2> GridComponent::GetShortestPath(const glm::vec2& startPos, const glm::vec2& endPos) const
+{
+    const int startNodeId = m_LevelGridGraph->GetNodeIdAtPosition(startPos);
+    const int endNodeId   = m_LevelGridGraph->GetNodeIdAtPosition(endPos);
+
+    return GetShortestPath(startNodeId, endNodeId);
+}
+
---
+++ Game/Components/GridComponent.hpp
@@ -0,0 +1,83 @@
+#pragma once
+
+#include "Base/DirectionEnum.hpp"
+#include "Components/Component.hpp"
+#include "Graphs/AStar.hpp"
+#include "Graphs/GridGraph.hpp"
+
+
+namespace Game
+{
+    class LevelGridGraph;
+
+    class GridComponent final : public bae::Component
+    {
+    public:
+        GridComponent(bae::GameObject& owner, int columns, int rows, const glm::ivec2& cellSize);
+        GridComponent(bae::GameObject& owner, const glm::vec2& gridSize, int columns, int rows);
+        ~GridComponent() override;
+
+
+        void Render() const override;
+
+        // Nodes
+        void AddNode(int nodeId) const;
+        void AddNode(const bae::Graphs::GridPosition& position) const;
+        void AddNodeAtPosition(const glm::vec2& positionNode) const;
+
+        void RemoveNode(int nodeId) const;
+        void RemoveNode(const bae::Graphs::GridPosition& position) const;
+        void RemoveNodeAtPosition(const glm::vec2& positionNode) const;
+
+        // Connections
+        void AddConnection(int nodeId, Direction direction) const;
+        void AddConnection(const glm::vec2& positionNode, Direction direction) const;
+
+        void RemoveConnection(int nodeId, Direction direction) const;
+        void RemoveConnection(const glm::vec2& positionNode, Direction direction) const;
+
+        void AddConnectionsToNeighbors(bae::Graphs::GridPosition position) const;
+
+
+        [[nodiscard]] bool GetRenderCells() const;
+        void SetRenderCells(bool bRenderCells) const;
+
+        [[nodiscard]] bool GetRenderNodes() const;
+        void SetRenderNodes(bool bRenderNodes) const;
+
+        [[nodiscard]] bool GetRenderConnections() const;
+        void SetRenderConnections(bool bRenderConnections) const;
+
+
+        [[nodiscard]] bae::Graphs::GridPosition GetGridPosition(const glm::vec2& position) const;
+
+        [[nodiscard]] glm::vec2 GetPosition(bae::Graphs::GridPosition gridPosition) const;
+
+        [[nodiscard]] bool IsValidGridPosition(bae::Graphs::GridPosition gridPosition) const;
+
+        [[nodiscard]] bae::Graphs::GridPosition GetClosestValidNodePosition(const glm::vec2& position) const;
+
+
+        [[nodiscard]] int GetColumns() const;
+        [[nodiscard]] int GetRows() const;
+
+
+        [[nodiscard]] bool IsInGrid(const glm::vec2& position) const;
+        [[nodiscard]] bool IsInGrid(const bae::Graphs::GridPosition& gridPosition) const;
+
+        [[nodiscard]] bool DoesConnectionExistInDirection(bae::Graphs::GridPosition gridPosition,
+                                                          Direction direction) const;
+
+        [[nodiscard]] std::vector<glm::vec2> GetALlConnections() const;
+
+
+        [[nodiscard]] std::vector<glm::vec2> GetShortestPath(int startNodeId, int endNodeId) const;
+        [[nodiscard]] std::vector<glm::vec2> GetShortestPath(const glm::vec2& startPos,
+                                                             const glm::vec2& endPos) const;
+
+    private:
+        std::unique_ptr<LevelGridGraph> m_LevelGridGraph;
+        bae::Graphs::AStar m_AStar;
+    };
+}
+
